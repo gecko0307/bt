f0989e506dc92814cdb8c759cb1d53a92b298fa0
Timur Gafarov <gecko0307@gmail.com>
2020-06-26 00:57:56 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
 const path = require("path");
+const { execute } = require("./utils");
+const builder = require("./builder");
 
 const cwd = process.cwd();
 const [,, ...args] = process.argv;
 //console.log(`${args}`);
 
-const builder = require("./builder");
-
 async function build()
 {
     console.log("BannerToolchain build");
-    const rollup = path.join(__dirname, "node_modules", ".bin", "rollup");
-    const rollupConfig = path.join(__dirname, "rollup.config.prod.js"); 
+    const rollup = path.join(__dirname, "..", "node_modules", ".bin", "rollup");
+    const rollupConfig = path.join(__dirname, "..", "rollup.config.prod.js"); 
     const code = await execute(rollup, ["-c", rollupConfig]);
     if (code === 0) {
         await builder("", "dist", "dist.zip");
@@ -25,8 +25,8 @@ async function build()
 async function run()
 {
     console.log("BannerToolchain run");
-    const rollup = path.join(__dirname, "node_modules", ".bin", "rollup");
-    const rollupConfig = path.join(__dirname, "rollup.config.dev.js");
+    const rollup = path.join(__dirname, "..", "node_modules", ".bin", "rollup");
+    const rollupConfig = path.join(__dirname, "..", "rollup.config.dev.js");
     const rollupCmd = `${rollup} -c ${rollupConfig} -m --watch`;
     const code = await execute(rollup, ["-c", rollupConfig, "-m", "--watch"]);
 }
