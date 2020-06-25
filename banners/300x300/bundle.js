00459fe174630d5c642ec5a4594c3c8417119c9f
Timur Gafarov <gecko0307@gmail.com>
2020-06-25 18:10:14 +0300

    center-h, center-v

--- banners/300x300/bundle.js
+++ banners/300x300/bundle.js
@@ -9,7 +9,7 @@
             autoplay: false
         });
         
-        tl.add({ targets: "#rect", duration: 1000, translateX: "900%", transformOrigin: ["center center 0", "center center 0"] });
+        tl.add({ targets: "#rect", duration: 1000, translateX: "450%", transformOrigin: ["center center 0", "center center 0"] });
         tl.add({ targets: "#rect", translateY: "900%", backgroundColor: "#0000ff" });
         tl.add({ targets: "#rect", duration: 500, transformOrigin: ["right bottom 0", "right bottom 0"],
             scaleX: 10.0,
--- banners/300x300/src/main.js
+++ banners/300x300/src/main.js
@@ -8,7 +8,7 @@ function mainTimeline()
         autoplay: false
     });
     
-    tl.add({ targets: "#rect", duration: 1000, translateX: "900%", transformOrigin: ["center center 0", "center center 0"] });
+    tl.add({ targets: "#rect", duration: 1000, translateX: "450%", transformOrigin: ["center center 0", "center center 0"] });
     tl.add({ targets: "#rect", translateY: "900%", backgroundColor: "#0000ff" });
     tl.add({ targets: "#rect", duration: 500, transformOrigin: ["right bottom 0", "right bottom 0"],
         scaleX: 10.0,
