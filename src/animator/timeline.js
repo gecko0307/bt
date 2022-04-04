ba9b112456981506593d40f6e0bb4fa96f1d5301
Timur Gafarov <gecko0307@gmail.com>
2022-04-04 17:31:19 +0300

    Cleanup

--- src/animator/timeline.js
+++ src/animator/timeline.js
@@ -1,20 +1,20 @@
 class Timeline {
-	constructor() {
-		this.tweens = [];
-	}
+    constructor() {
+        this.tweens = [];
+    }
 
-	fromTo(selector, duration, propsFrom, propsTo, time) {
-		const tween = {
-			selector: selector,
-			duration: duration,
-			propsFrom: propsFrom,
-			propsTo: propsTo,
-			time: time
-		};
-		this.tweens.push(tween);
-	}
+    fromTo(selector, duration, propsFrom, propsTo, time) {
+        const tween = {
+            selector: selector,
+            duration: duration,
+            propsFrom: propsFrom,
+            propsTo: propsTo,
+            time: time
+        };
+        this.tweens.push(tween);
+    }
 }
 
 module.exports = {
-	Timeline
+    Timeline
 };
