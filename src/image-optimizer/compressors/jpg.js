086c9a6d2b8a50e3348cf5451a9511b2f756a975
Тимур Гафаров <gt@smarthead.ru>
2022-04-25 14:10:03 +0300

    Cleanup

--- src/image-optimizer/compressors/jpg.js
+++ src/image-optimizer/compressors/jpg.js
@@ -3,14 +3,16 @@ const { spawnAsStream } = require("../utils");
 
 async function compress(inputStream, options) {
     const compressOpts = options.options.compress;
-	// MozJPEG compressor stream
-	const mjpegArgs = [];
-	if (options.quality) mjpegArgs.push("-quality", options.quality);
-	if (compressOpts.grayscale) mjpegArgs.push("-grayscale");
-	if (compressOpts.progressive) mjpegArgs.push("-progressive");
-	else mjpegArgs.push("-baseline");
-	const jpegStream = inputStream.pipe(spawnAsStream(mozjpeg.cjpeg, mjpegArgs));
-    return jpegStream;
+
+    // MozJPEG compressor stream
+    const mjpegArgs = [];
+    if (options.quality) mjpegArgs.push("-quality", options.quality);
+    if (compressOpts.grayscale) mjpegArgs.push("-grayscale");
+    if (compressOpts.progressive) mjpegArgs.push("-progressive");
+    else mjpegArgs.push("-baseline");
+    let outStream = inputStream.pipe(spawnAsStream(mozjpeg.cjpeg, mjpegArgs));
+
+    return outStream;
 }
 
 module.exports = compress;
