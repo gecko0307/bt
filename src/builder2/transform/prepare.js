dfba519d494e72ba80d1c78858924fac3ee4154a
Тимур Гафаров <gt@smarthead.ru>
2022-05-23 18:23:44 +0300

    Reduce redundancy

--- src/builder2/transform/prepare.js
+++ src/builder2/transform/prepare.js
@@ -10,41 +10,41 @@ async function prepare(filename, document, tr, options) {
         banner: options.banner
     };
 
-    if ("head" in tr.tags) {
-        head.appendChild(document.createTextNode("\r\n"));
-        for (const tag of tr.tags.head) {
-            let element;
-            if (tag.tag === "comment") {
-                const template = tag.text || "";
-                const value = Mustache.render(template, templateData);
-                element = document.createComment(value);
-            }
-            else {
-                element = document.createElement(tag.tag);
-                for (const name of Object.keys(tag.attributes)) {
-                    const template = tag.attributes[name];
-                    const value = Mustache.render(template, templateData);
-                    element.setAttribute(name, value);
-                }
-                if (tag.text) element.innerHTML = tag.text;
-            }
-            head.appendChild(element);
-            head.appendChild(document.createTextNode("\r\n"));
+    function addTag(tag, rootElement) {
+        let element;
+        if (tag.tag === "comment") {
+            const template = tag.text || "";
+            const value = Mustache.render(template, templateData);
+            element = document.createComment(value);
         }
-    }
-
-    if ("body" in tr.tags) {
-        head.appendChild(document.createTextNode("\r\n"));
-        for (const tag of tr.tags.body) {
-            const element = document.createElement(tag.tag);
-            if (tag.text) element.innerHTML = tag.text;
+        else {
+            element = document.createElement(tag.tag);
             for (const name of Object.keys(tag.attributes)) {
                 const template = tag.attributes[name];
                 const value = Mustache.render(template, templateData);
                 element.setAttribute(name, value);
             }
-            body.appendChild(element);
-            head.appendChild(document.createTextNode("\r\n"));
+            if (tag.text) {
+                const template = tag.text;
+                const value = Mustache.render(template, templateData);
+                element.innerHTML = value;
+            }
+        }
+        rootElement.appendChild(element);
+        rootElement.appendChild(document.createTextNode("\r\n"));
+    }
+
+    if ("head" in tr.tags) {
+        head.appendChild(document.createTextNode("\r\n"));
+        for (const tag of tr.tags.head) {
+            addTag(tag, head);
+        }
+    }
+
+    if ("body" in tr.tags) {
+        body.appendChild(document.createTextNode("\r\n"));
+        for (const tag of tr.tags.body) {
+            addTag(tag, body);
         }
     }
 
