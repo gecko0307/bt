66c962945ecf758bdf59c14142de11c6a85f885a
Timur Gafarov <gecko0307@gmail.com>
2025-05-19 01:41:22 +0300

    Generate .data/builder.config.json for new projects

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 const fs = require("fs-extra");
 const path = require("path");
 const opener = require("opener");
+const Git = require("simple-git");
 const server = require("./server");
 const capturer = require("./capturer");
 const gulpBuilder = require("./gulp-builder");
 const nativeBuilder = require("./builder2");
 const deployer = require("./deployer");
 const runRollup = require("./rollup");
+const parseRepoUrl = require("./deployer/parseRepoUrl");
+const parseBranch = require("./deployer/parseBranch");
 
 const cwd = process.cwd();
 const [,, ...args] = process.argv;
@@ -85,12 +88,79 @@ async function run(options = {}) {
     }
 }
 
+function getCurrentBranch(git) {
+    return new Promise((resolve, reject) => {
+        git.branch(function(err, res){
+            if (err) reject(err);
+            let current = null;
+            for (let name in res.branches) {
+                if (res.branches[name].current) {
+                    current = res.branches[name];
+                    break;
+                }
+            }
+            resolve(current);
+        });
+    });
+}
+
+function getBranchCreative(branchData) {
+    return branchData.creative || "";
+}
+
+function getBranchSize(branchData) {
+    let size = "";
+    if (branchData.props) {
+        if (branchData.props.width && branchData.props.height) {
+            let w = branchData.props.width;
+            let h = branchData.props.height;
+            if (branchData.props.minWidth) {
+                const minW = branchData.props.minWidth;
+                w += `(${minW}`;
+                if (branchData.props.maxWidth) {
+                    const maxW = branchData.props.maxWidth;
+                    w += `-${maxW})`;
+                }
+                else
+                    w += ")";
+            }
+            if (branchData.props.minHeight) {
+                const minH = branchData.props.minHeight;
+                h += `(${minH}`;
+                if (branchData.props.maxHeight) {
+                    const maxH = branchData.props.maxHeight;
+                    h += `-${maxH})`;
+                }
+                else
+                    h += ")";
+            }
+            size = `${w}x${h}`;
+        }
+    }
+    return size;
+}
+
+function getBranchPlatform(branchData) {
+    let platform = "publish";
+    if (branchData.platform && branchData.platform.length > 0)
+        platform = branchData.platform;
+    return platform;
+}
+
+function getBranchVersion(branchData) {
+    let version = "v1";
+    if (branchData.version && branchData.version.length > 0)
+        version = branchData.version;
+    return version;
+}
+
 async function init(options = { template: "default" }) {
     console.log("BannerToolchain init");
     
     const template = options.template || "default";
     const templatePath = path.join(__dirname, "..", "templates", template);
     if (await fs.pathExists(templatePath)) {
+        console.log(`Initializing a project with template "${template}"...`);
         const destPath = path.join(cwd);
         fs.copySync(templatePath, destPath);
         
@@ -103,9 +173,44 @@ async function init(options = { template: "default" }) {
             console.log(`Set package verison to "${projectConfig.version}"`);
         }
         
-        // TODO: generate .data/builder.config.json
+        // Generate .data/builder.config.json
+        const git = Git(cwd);
+        git.outputHandler(function(command, stdout, stderr) {
+            //stdout.pipe(process.stdout);
+            stderr.pipe(process.stderr);
+        });
+        if (await git.checkIsRepo()) {
+            console.log("Git repo found");
+            let url = (await git.raw(["config", "--get", "remote.origin.url"])).trim();
+            if (url.endsWith(".git")) {
+                url = url.substring(0, url.length - 4);
+            }
+            const repoInfo = parseRepoUrl(url);
+            const projectName = repoInfo.project;
+            const branch = await getCurrentBranch(git);
+            const branchData = parseBranch(branch.name);
+            
+            console.log(`Project: ${projectName}`);
+            console.log(`Branch: ${branch.name}`);
+            
+            const builderConfig = {
+                brand: projectName,
+                campaign: "", // ?
+                creative: getBranchCreative(branchData),
+                platform: getBranchPlatform(branchData),
+                size: getBranchSize(branchData),
+                version: getBranchVersion(branchData)
+            };
+            
+            const dataPath = path.resolve(".data");
+            await fs.mkdir(dataPath, { recursive: true });
+            const filePath = path.join(dataPath, "builder.config.json");
+            const builderConfigJson = JSON.stringify(builderConfig, null, '\t');
+            await fs.writeFile(filePath, builderConfigJson, "utf8");
+            console.log("Update .data/builder.config.json");
+        }
         
-        console.log(`Initialized a project with template "${template}"`);
+        console.log(`Done!`);
     }
     else {
         console.log(`Error: template "${template}" not found!`);
